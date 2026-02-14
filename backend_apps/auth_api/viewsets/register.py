2e152ba8f0c47f3d55f370ed8e369c66208d60f9
vlad <de_vlad@gmx.com>
2026-02-14 23:51:44 +0300

    commit before connecting the Elasticsearch search model

--- backend_apps/auth_api/viewsets/register.py
+++ backend_apps/auth_api/viewsets/register.py
@@ -1,8 +1,6 @@
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ViewSet
-from rest_framework_simplejwt.tokens import RefreshToken
 
 from backend_apps.auth_api.serializers.register import RegisterSerializer
 
